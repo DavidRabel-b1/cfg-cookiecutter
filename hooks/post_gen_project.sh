575ee2eb1d1074151c0f7fcce088e3c055bb26b9
Christian Berendt <berendt@betacloud-solutions.de>
2018-07-27 14:57:27 +0200

    kolla: add horizon local_settings.j2 template file

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,6 +3,9 @@
 mv environments/kolla/files/overlays/haproxy/haproxy.cfg.{{ cookiecutter.openstack_version }} environments/kolla/files/overlays/haproxy/haproxy.cfg
 rm -f environments/kolla/files/overlays/haproxy/haproxy.cfg.*
 
+mv environments/kolla/files/overlays/horizon/local_settings.j2.{{ cookiecutter.openstack_version }} environments/kolla/files/overlays/horizon/local_settings.j2
+rm -f environments/kolla/files/overlays/horizon/local_settings.j2.{{ cookiecutter.openstack_version }}
+
 mkdir secrets
 for name in operator configuration; do
     ssh-keygen -t rsa -b 4096 -N "" -f secrets/id_rsa.$name -C ""
